6fd868498a90e7180662e0389d18c33b1ba406fc
Wright <calebogundiya@gmail.com>
2023-02-19 12:20:01 +0000

    Client game create handler

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 const http = require('http');
 const app = require("express")();
 
+// Load static pages
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 app.listen(9091, () => console.log("Frontend on port 9091"));
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 const websockerServer = require('websocket').server;
 
 const httpServer = http.createServer();
 httpServer.listen(9090, () => console.log("Listening on 9090"))
 
-const clients = {};
-
+// Upgrade http server to websocket server
 const wsServer = new websockerServer({
     httpServer
 })
 
+const clients = {};
+const games = {};
+
 wsServer.on('request', req => {
     // connect
-    const connection = req.accept(null, request.origin)
+    const connection = req.accept(null, req.origin)
     connection.on('open', () => console.log('OPENED!'))
     connection.on('close', () => console.log('CLOSED!'))
     connection.on('message', message => {
         const result = JSON.parse(message.utf8Data)
-        console.log(result);
+
+        // Received message from client
+        // user wants to create a new game
+        if (result.method = 'create') {
+
+            const clientId = result.clientId
+            const gameId = guid();
+            games[gameId] = {
+                id: gameId,
+                balls: 20
+            }
+
+            const payLoad = {
+                method: 'create',
+                game: games[gameId]
+            }
+
+            const con = clients[clientId].connection
+            con.send(JSON.stringify(payLoad));
+        }
     })
 
     // Generate a new clientId
@@ -42,7 +64,9 @@ wsServer.on('request', req => {
 })
 
 function s4() {
-    Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return Math.floor(
+        (1 + Math.random()) * 0x10000
+    ).toString(16).substring(1);
 }
 
 const guid = () => `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4() + s4() + s4()}`;
